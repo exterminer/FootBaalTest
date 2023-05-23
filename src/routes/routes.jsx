980754a639e0a036fcd04204f1efe9a44d3da1c4
igor <igorbragafarais@gmail.com>
2023-05-23 14:56:53 -0300

    gravaçaoxpto

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home/home";
+import { Dash } from "../pages/DashBoard/Dash";
 
 export function RouterMain() {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}></Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/dashboard" element={<Dash />}></Route>
     </Routes>
   );
 }
